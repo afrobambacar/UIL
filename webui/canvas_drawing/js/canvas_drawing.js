954c553955cd822f361a12f6b1fa99b659d0662a
jwseo <pppmm86@gmail.com>
2016-02-12 18:53:34 +0900

    fix canvas 01

--- webui/canvas_drawing/js/canvas_drawing.js
+++ webui/canvas_drawing/js/canvas_drawing.js
@@ -1,65 +1,19 @@
 var canvas;
 var ctx;
 var flag = false;
-var mobileInfo = new Array('iPhone', 'Android', 'iPad');
 
 $(document).ready(function() {
 
 	canvas = document.getElementById('canvas');
 	ctx = canvas.getContext("2d");
 
-	//로드 이미지 캔버스에 보이기
 	document.getElementById("loadImg").addEventListener("change", loadImg, false);
 
-	for (var info in mobileInfo) {
-		if (navigator.userAgent.match(mobileInfo[info]) != null || document.body.clientWidth < 600) {
-			var topH = parseInt($(".top").css("height"));
-			canvas.width = window.innerWidth - 10;
-			canvas.height = window.innerHeight - (topH + 10);
-			canvas.addEventListener("touchstart", touchStart, false);
-			canvas.addEventListener("touchmove", touchMove, false);
-			canvas.addEventListener("touchend", touchEnd, false);
-			btns();
-		} else {
-			webReady();
-			btns();
-		}
-	}
-
-	//캔버스 화면 흰바탕 깔기
-	ctx.beginPath();
-	ctx.rect (0,0, canvas.width, canvas.height);
-	ctx.fillStyle = '#ffffff';
-	ctx.fill();
+	webReady();
+	btns();
 });
 
-//모바일일때
-function touchStart(event) {
-	var touch = event.targetTouches[0];
-
-	ctx.beginPath();
-	ctx.moveTo(touch.pageX, touch.pageY);
-
-	flag = true;
-	
-}
-function touchMove(event) {
-	event.preventDefault();
-
-	if (flag) {
-		ctx.lineTo(event.targetTouches[0].pageX, event.targetTouches[0].pageY);
-		ctx.stroke();
-	}
-	
-}
-function touchEnd(event) {
-
-	ctx.closePath();
-	flag = false;
-	
-}
-
-//웹일때
+//web
 function webReady() {
 
 	canvas.onmousedown = drawStart;
@@ -81,33 +35,12 @@ function loadImg (event) {
 	}
 }
 
-
-function drawStart(event) {
-	var xpos = event.target.offsetLeft;
-	var ypos = event.target.offsetTop;
-	ctx.moveTo(event.clientX-xpos, event.clientY-ypos);
-	flag = true;
-}
-function drawing(event) {
-	if (flag) {
-		var xpos = event.target.offsetLeft;
-		var ypos = event.target.offsetTop;
-		ctx.lineCap = "round";
-		ctx.lineTo(event.clientX-xpos, event.clientY-ypos);
-		ctx.stroke();
-	}
-}
-function drawEnd(event) {
-	flag = false;
-}
-
-
-//버튼들
-function btns(e) {
+//버튼
+function btns() {
 	$(".btn-save").click(function() {
 		window.open(canvas.toDataURL());
 	});
-	$(".btn-new").click(function(e) {
+	$(".btn-new").click(function() {
 		ctx.clearRect(0, 0, canvas.width, canvas.height);
 	});
 	$(".btn-bold").click(function() {
@@ -137,9 +70,27 @@ function btns(e) {
 	$(".btn-yellow").click(function() {
 		ctx.strokeStyle = "#EFFF00";
 	});
-	// $(".btn-eraser").click(function() {
-	// 	ctx.strokeStyle = "#ffffff";
-	// });
 }
 
+function drawStart(event) {
+	ctx.beginPath();
+	var xpos = event.target.offsetLeft;
+	var ypos = event.target.offsetTop;
+	ctx.moveTo(event.clientX-xpos, event.clientY-ypos);
+	flag = true;
+}
+function drawing(event) {
+	if (flag) {
+		var xpos = event.target.offsetLeft;
+		var ypos = event.target.offsetTop;
+		ctx.lineCap = "round";
+		ctx.lineTo(event.clientX-xpos, event.clientY-ypos);
+		ctx.stroke();
+	}
+}
+function drawEnd(event) {
+	flag = false;
+}
+
+	
 
